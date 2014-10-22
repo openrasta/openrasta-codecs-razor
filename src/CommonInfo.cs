340b86871a4b30d54b2781791b12c1dc527d602c
Toby Henderson <toby.henderson@huddle.com>
2014-10-22 17:43:36 +0100

    Added Common Assembly Info

---
+++ src/CommonInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0")]
+
+[assembly: AssemblyCompany("HP and Caffeine IT- naughtyProd Limited")]
+[assembly: AssemblyProduct("OpenRasta Razor")]
+[assembly: AssemblyCopyright("Copyright © HP 2011 and Copyright (c) Caffeine IT - naughtyProd Limited - Sebastien Lambla")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
