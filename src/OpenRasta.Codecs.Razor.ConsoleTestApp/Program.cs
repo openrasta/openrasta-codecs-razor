25036252cb62df3af68e1d3c97e6f9e762f545c2
Toby Henderson <toby.henderson@huddle.com>
2014-10-22 16:17:20 +0100

    Upgraded solution and project files
    All .net 4.0

--- src/OpenRasta.Codecs.Razor.ConsoleTestApp/Program.cs
+++ src/OpenRasta.Codecs.Razor.ConsoleTestApp/Program.cs
@@ -9,8 +9,17 @@ namespace OpenRasta.Codecs.Razor.ConsoleTestApp
             var h = new HttpListenerHostWithConfiguration { Configuration = new Configuration() };
             h.Initialize(new[] { "http://+:9222/" }, "/", null);
             h.StartListening();
+
+
+            h.IncomingRequestReceived += h_IncomingRequestReceived;
+
             Console.WriteLine("Press enter to exit.");
             Console.ReadLine();
+        }
+
+        static void h_IncomingRequestReceived(object sender, Hosting.IncomingRequestReceivedEventArgs e)
+        {
+            Console.WriteLine("Incoming request: " + e.Context.Request.Uri);
         }
     }
 }
